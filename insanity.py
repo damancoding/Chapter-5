5a2dd47eb95eb24dec71c87af6b6f1fcc0438937
Amanda M <156499790+damancoding@users.noreply.github.com>
2024-02-14 17:29:51 -0500

    Add files via upload

---
+++ insanity.py
@@ -0,0 +1,10 @@
+# this loop is insanity, repetiting a number
+# multiple times without changing the outcome
+# Python doesn't have a Do - Until loop
+# 2/14/2024
+# Amanda M
+
+num = float(input("Enter a number: "))
+for i in range(1, 11):
+    print(num)
+
